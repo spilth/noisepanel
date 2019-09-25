f4b4ee04ff2cd165658fdd7c5fb5b77341af997c
Brian Kelly <bkelly@carbonfive.com>
2019-09-24 20:10:35 -0400

    Simple audio player component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { FC } from "react";
 import "./App.scss";
+
+interface SoundEffectProps {
+  name: string;
+  path: string;
+};
+const SoundEffect: FC<SoundEffectProps> = props => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <audio controls src={props.path} />
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <div>
-      <audio controls src="/sounds/price-is-wrong.mp3" />
+      <SoundEffect name="Price Is Wrong" path="/sounds/price-is-wrong.mp3" />
+      <SoundEffect name="Dun dun duuun!!!" path="/sounds/dun-dun-duuun.mp3" />
     </div>
   );
 };
