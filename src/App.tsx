6b9bc0c6da08bbed083470bed697dfe6045309dc
Brian Kelly <bkelly@carbonfive.com>
2019-09-25 12:19:31 -0400

    Add all sounds

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,92 @@ import SoundEffect from "./components/SoundEffect/SoundEffect";
 const App: React.FC = () => {
   return (
     <div className="sound-effects">
-      <SoundEffect name="Price Is Wrong" path="/sounds/price-is-wrong.mp3" />
-      <SoundEffect name="Dun dun duuun!!!" path="/sounds/dun-dun-duuun.mp3" />
+      <SoundEffect name="B17 Bomber" path="/sounds/b17-bomper.mp3" />
+      <SoundEffect name="Ba Dum Dum Tss" path="/sounds/ba-dum-tss.mp3" />
+      <SoundEffect name="Bat Signal" path="/sounds/bat-signal.mp3" />
+      <SoundEffect name="Bored Bored Bored" path="/sounds/b-b-b-bored.mp3" />
+      <SoundEffect name="Canned Laughter" path="/sounds/canned-laughter.mp3" />
+      <SoundEffect
+        name="Captain Refactor"
+        path="/sounds/captain-refactor.mp3"
+      />
+      <SoundEffect name="Car Horn" path="/sounds/car-horn.mp3" />
+      <SoundEffect
+        name="Charlie Brown Teacher"
+        path="/sounds/charlie-brown-teacher.mp3"
+      />
+      <SoundEffect
+        name="Close Encounters"
+        path="/sounds/close-encounters.mp3"
+      />
+      <SoundEffect name="Creaky Door" path="/sounds/creaky-door.mp3" />
+      <SoundEffect name="Creepy (Long)" path="/sounds/creepy-long.mp3" />
+      <SoundEffect name="Creepy (Short)" path="/sounds/creepy-short.mp3" />
+      <SoundEffect name="Crickets" path="/sounds/crickets.mp3" />
+      <SoundEffect name="DJ Horn" path="/sounds/dj-horn.mp3" />
+      <SoundEffect name="Doorbell" path="/sounds/doorbell.mp3" />
+      <SoundEffect name="Dream Sequence" path="/sounds/dream-sequence.mp3" />
+      <SoundEffect name="Dun Dun Duuun" path="/sounds/dun-dun-duuun.mp3" />
+      <SoundEffect name="Gibberish" path="/sounds/gibberish.mp3" />
+      <SoundEffect name="Excuse me, Princess!" path="/sounds/excuse-me.mp3" />
+      <SoundEffect
+        name="Get to the Choppa!"
+        path="/sounds/get-to-the-choppa.mp3"
+      />
+      <SoundEffect
+        name="Granfather Clock"
+        path="/sounds/grandfather-clock.mp3"
+      />
+      <SoundEffect name="Ha ha!" path="/sounds/ha-ha.mp3" />
+      <SoundEffect name="I'm bored" path="/sounds/i-m-bored.mp3" />
+      <SoundEffect name="Jaws Theme" path="/sounds/jaws.mp3" />
+      <SoundEffect
+        name="Jeopardy Countdown"
+        path="/sounds/jeopardy-countdown.mp3"
+      />
+      <SoundEffect name="LOST" path="/sounds/lost-suspense.mp3" />
+      <SoundEffect name="Mario's Death" path="/sounds/mario-death.mp3" />
+      <SoundEffect
+        name="No! God please, no!"
+        path="/sounds/no-god-please-no.mp3"
+      />
+      <SoundEffect name="Nooooo!" path="/sounds/darth-no.mp3" />
+      <SoundEffect name="Old Phone" path="/sounds/old-phone-ringing.mp3" />
+      <SoundEffect name="Orchestra Hit" path="/sounds/orchestra-hit.mp3" />
+      <SoundEffect name="Ownage!" path="/sounds/ownage.wav" />
+      <SoundEffect name="Price is Right" path="/sounds/price-is-right.mp3" />
+      <SoundEffect name="Price is Wrong" path="/sounds/price-is-wrong.mp3" />
+      <SoundEffect name="Record Scratch" path="/sounds/record-scratch.mp3" />
+      <SoundEffect name="Reveille" path="/sounds/reveille.mp3" />
+      <SoundEffect name="Sad Trombone" path="/sounds/sad-trombone.mp3" />
+      <SoundEffect name="Serial Theme" path="/sounds/serial-theme.mp3" />
+      <SoundEffect name="Snoring (Cartoon)" path="/sounds/snoring-funny.mp3" />
+      <SoundEffect
+        name="Star Trek Announcement"
+        path="/sounds/tos_shipannouncement.mp3"
+      />
+      <SoundEffect
+        name="Stop the insanity!"
+        path="/sounds/stop-the-insanity.mp3"
+      />
+      <SoundEffect
+        name="Stranger Things Theme"
+        path="/sounds/stranger-things.mp3"
+      />
+      <SoundEffect name="Thunder" path="/sounds/thunder.mp3" />
+      <SoundEffect name="THX" path="/sounds/thx.mp3" />
+      <SoundEffect
+        name="What a Concept"
+        path="/sounds/yup-what-a-concept.mp3"
+      />
+      <SoundEffect
+        name="Where did you learn to fly?"
+        path="/sounds/where-did-you-learn-to-fly.mp3"
+      />
+      <SoundEffect name="Wilhelm Scream" path="/sounds/wilhelm.mp3" />
+      <SoundEffect name="Yakety Sax" path="/sounds/yakety-sax.mp3" />
+      <SoundEffect name="Yes!" path="/sounds/yes.mp3" />
+      <SoundEffect name="You're the man now, dog!" path="/sounds/ytmnd.mp3" />
     </div>
   );
 };
