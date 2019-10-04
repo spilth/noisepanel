56afb6e1a51617fad472ad488559eb6b719fbb5b
Brian Kelly <polymonic@gmail.com>
2019-10-03 22:09:52 -0400

    Add play/stop buttons and relax/reset the styling

--- src/components/SoundEffect/SoundEffect.tsx
+++ src/components/SoundEffect/SoundEffect.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from "react";
+import React, { FC, useRef, useState } from "react";
 
 import "./SoundEffect.scss";
 
@@ -9,6 +9,7 @@ interface SoundEffectProps {
 
 const SoundEffect: FC<SoundEffectProps> = props => {
   const audioElement = useRef(null);
+  const [playing, setPlaying] = useState(false);
 
   const playAudio = () => {
     if (audioElement !== null) {
@@ -16,17 +17,57 @@ const SoundEffect: FC<SoundEffectProps> = props => {
         audioElement.current || new HTMLAudioElement();
 
       if (current !== null) {
-        current.play().then(r => {});
+        current.play().then(() => {});
+        setPlaying(true);
       }
     }
   };
 
+  const pauseAudio = () => {
+    if (audioElement !== null) {
+      const current: HTMLAudioElement =
+        audioElement.current || new HTMLAudioElement();
+
+      if (current !== null) {
+        current.pause();
+        setPlaying(false);
+      }
+    }
+  };
+
+  const stopAudio = () => {
+    if (audioElement !== null) {
+      const current: HTMLAudioElement =
+        audioElement.current || new HTMLAudioElement();
+
+      if (current !== null) {
+        current.load();
+        setPlaying(false);
+      }
+    }
+  };
+
+  const audioFinished = () => {
+    setPlaying(false);
+  };
+
   return (
-    <div>
-      <button className="sound-effect__button" onClick={playAudio}>
-        {props.name}
-      </button>
-      <audio src={props.path} ref={audioElement} />
+    <div className="sound-effect">
+      <p>{props.name}</p>
+      <p>
+        {!playing && (
+          <button className="sound-effect__button" onClick={playAudio}>
+            <i className="fas fa-play"></i>
+          </button>
+        )}
+        {playing && (
+          <button className="sound-effect__button" onClick={stopAudio}>
+            <i className="fas fa-stop"></i>
+          </button>
+        )}
+      </p>
+
+      <audio src={props.path} ref={audioElement} onEnded={audioFinished} />
     </div>
   );
 };
